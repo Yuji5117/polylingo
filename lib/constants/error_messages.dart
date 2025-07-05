235aad56019bbc7209385190c794a998186f2d5f
Yuji Nakamura <ny5117@icloud.com>
2025-07-05 14:09:57 +0700

    Add error messages constants

---
+++ lib/constants/error_messages.dart
@@ -0,0 +1,7 @@
+class ErrorMessages {
+  static const String network =
+      'Unable to connect to the network. Please check your internet connection.';
+  static const String timeout = 'The request timed out. Please try again.';
+  static const String server =
+      'A server error occurred. Please try again later.';
+}
