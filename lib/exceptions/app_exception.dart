5b8c0f7e7cc832026cf2e4d64f3f4d8b1eda0a3e
Yuji Nakamura <ny5117@icloud.com>
2025-07-05 14:45:05 +0700

    Add AppException

---
+++ lib/exceptions/app_exception.dart
@@ -0,0 +1,18 @@
+import 'package:polylingo/constants/error_messages.dart';
+
+abstract class AppException implements Exception {
+  final String message;
+  const AppException(this.message);
+}
+
+class NetworkException extends AppException {
+  const NetworkException() : super(ErrorMessages.network);
+}
+
+class TimeoutAppException extends AppException {
+  const TimeoutAppException() : super(ErrorMessages.timeout);
+}
+
+class ServerException extends AppException {
+  const ServerException() : super(ErrorMessages.server);
+}
